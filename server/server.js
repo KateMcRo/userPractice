85577a9ad5eb3c62ba086e8b9d2ce00ca63ef6f0
KateMcRo <katemcr0@gmail.com>
2023-12-01 16:09:10 -0800

    initial server set up

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Listening on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
