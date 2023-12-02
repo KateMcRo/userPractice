40ac38407831bb9483f9bf7fb3ec9c2251db5996
KateMcRo <katemcr0@gmail.com>
2023-12-02 12:51:25 -0800

    added password hashing

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -14,6 +16,17 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  try {
+    const hashed = await bcrypt.hash(this.password, 10);
+    this.password = hashed;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);
